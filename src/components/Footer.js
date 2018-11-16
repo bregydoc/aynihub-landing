ea506d8625acc344fc6391cd7f83def158a0275c
Bregy <bregy.malpartida@utec.edu.pe>
2018-11-16 10:29:45 -0500

    aaa

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,47 @@
 import React from "react";
 import styled from "styled-components";
-import { Card, Heading, Image } from "rebass";
+import { Card, Text, Image, Box, Flex } from "rebass";
+import miniAyni from '../images/aynihub_mini.png'
+import fb from '../images/facebook.png';
+import twt from '../images/twitter.png';
 
 const Footer = props => {
-  return <div>FOOTER</div>;
+  return <Flex
+    p={3}
+    width={"100vw"}
+    color='white'
+    bg='#01070F'
+    justifyConten={"space-between"}
+
+  >
+    <Box width={[1, 1/3]} p={2}>
+      <Image width={[0.3]} src={miniAyni}/>
+      <Text fontFamily={"Nunito Sans"}  py={3} ml={2} pr={4} letterSpacing={1}>
+        Aynihub está en desarrollo, si tienes alguna idea de lo que quisieras ver en Aynihub, no dudes en contactarnos
+      </Text>
+      <Text fontFamily={"Nunito Sans"}  py={3} ml={2} letterSpacing={1}>
+        +51957821858
+      </Text>
+      <Text fontFamily={"Nunito Sans"}  py={3} ml={2} letterSpacing={1}>
+        bregy.malpartida@utec.edu.pe
+      </Text>
+
+
+    </Box>
+    <Box width={[1, 1/3]}>
+      <Flex>
+        <Box width={1/2}>
+          <Image src={fb} width={[0.06]}/>
+        </Box>
+        <Box width={1/2}>
+          <Image src={twt} width={[0.1]}/>
+        </Box>
+      </Flex>
+    </Box>
+      <Box width={[1, 1/3]}>
+        {" "}
+      </Box>
+  </Flex>;
 };
 
 export default Footer;
--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -1,9 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 import { Card, Heading, Image } from "rebass";
+import MailchimpSubscribe from "react-mailchimp-subscribe"
+
+const url = "//xxxx.us13.list-manage.com/subscribe/post?u=zefzefzef&id=fnfgn";
 
 const Subscribe = props => {
-  return <div>Subscribe</div>;
+  return <div>
+    <MailchimpSubscribe
+      url={url}
+      render={({ subscribe, status, message }) => (
+        <div>
+          <MyForm onSubmitted={formData => subscribe(formData)} />
+          {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
+          {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{__html: message}}/>}
+          {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
+        </div>
+      )}
+    />
+  </div>;
 };
 
 export default Subscribe;
